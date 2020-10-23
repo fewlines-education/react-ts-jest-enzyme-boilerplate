c1a44d78d83ea314d2d72f677275c329f168047d
Fuzznimp <montaigne.anthony.dev@gmail.com>
2020-10-23 12:47:01 +0200

    Added react/ts app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles/app.css";
+
+export const App: React.FC = () => {
+  return (
+    <div id="app" className="container default-flex">
+      <h1>Happy coding 💪</h1>
+    </div>
+  );
+};
